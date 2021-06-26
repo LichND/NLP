8e0949fc11d8b7ff36c07763ece79d4af9caa90c
lichND <lichnd99@gmail.com>
2021-06-26 11:58:50 +0700

    remove .vec file

--- train.js
+++ train.js
@@ -5,7 +5,10 @@ const rimraf = require("rimraf")
 
 rimraf.sync(constant.outDir)
 fs.mkdirSync(constant.outDir)
-fs.exists(constant.dataDir + constant.tagFile, exists => exists && fs.copyFile(constant.dataDir + constant.tagFile, constant.outDir + constant.tagFile, constant.callback))
+
+let copyIfExists = (src, dest) => fs.exists(src, res => res && fs.copyFile(src, dest, constant.callback))
+copyIfExists(constant.dataDir + constant.tagFile, constant.outDir + constant.tagFile)
+copyIfExists(constant.dataDir + constant.keyword, constant.outDir + constant.keyword)
 
 let input = [constant.dataDir + constant.topicFile, constant.dataDir + constant.typeFile, constant.dataDir + constant.regularFile]
 let output = [constant.outDir + constant.topicFile, constant.outDir + constant.typeFile, constant.outDir + constant.regularFile]
@@ -24,8 +27,14 @@ const train = (input, output, callback) => {
 }
 
 const callFunc = (input, ouput, index) => {
-    if (input[index] == null || ouput[index] == null)
+    if (input[index] == null || ouput[index] == null) {
+        fs.readdir(constant.outDir, (err, files) => {
+            if (err)
+                throw err
+            files.forEach(e => e.endsWith(".vec") && fs.unlink(constant.outDir + e, constant.callback))
+        })
         return
+    }
     train(input[index], ouput[index], () => {
         console.log(`#${index + 1} train complete!!`)
         callFunc(input, output, index + 1)
